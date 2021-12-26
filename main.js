783f2480a4d8b95ed9bac1a4182a59a1a2c951a9
Pawandeep singh <sirpawandeep25@gmail.com>
2021-12-26 19:49:45 +0530

    Major Update
    Full UI updated

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
+//podcast section
 var abc=document.querySelector(".abc");
 var audioo = document.querySelector("audio");
 
 let tracksObj = {
 	track1: {
-        name: "Mitwa",
-        coverArt: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NYCS-bull-trans-1.svg/1024px-NYCS-bull-trans-1.svg.png",
-        soundfile: "soundfiles/mitwa.mp3",
+        name: "IS IT WORTH IT?",
+        coverArt: "pics/I Got You Cover Art.png",
+        soundfile: "soundfiles/is it worth it.mp3",
     },
     track2: {
-        name: "Pani Da Rang",
-        coverArt: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/NYCS-bull-trans-2.svg/1200px-NYCS-bull-trans-2.svg.png",
-        soundfile: "soundfiles/Pani da rang.mp3",
-    },
-    track3: {
-        name: "Sapna Jahan",
-        coverArt: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/NYCS-bull-trans-3.svg/1200px-NYCS-bull-trans-3.svg.png",
-        soundfile: "soundfiles/sapna.mp3",
-    },
-   
-    track4: {
-        name: "Teri Hogaiyaan 2",
-        coverArt: "pics/I Got You Cover Art.png",
-        soundfile: "soundfiles/Teri Hogaiyaan.mp3",
-    },
-    track5: {
-        name: "Baarish",
-        coverArt: "pics/evenNewStrict.png",
-        soundfile: "soundfiles/baarish.mp3",
-    },
-    track6: {
-        name: "Jab Tak",
+        name: "TRUST",
         coverArt: "pics/AuraCoverArt.png",
-        soundfile: "soundfiles/jabtak.mp3",
+        soundfile: "soundfiles/trust.mp3",
     },
-    track7: {
-        name: "Kuch to hai",
-        coverArt: "pics/I Got You Cover Art.png",
-        soundfile: "soundfiles/kuchtohai.mp3",
-    },
-
     
 }
 
@@ -163,4 +137,168 @@ $("#down").click(function() {
     audioo.loop=false;
   audioo.play();
 
+});
+
+
+
+
+
+
+
+
+
+// Mythium Archive: https://archive.org/details/mythium/
+//for music section
+
+jQuery(function ($) {
+    'use strict'
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        // initialize plyr
+        var player = new Plyr('#audio1', {
+            controls: [
+                'restart',
+                'play',
+                'progress',
+                'current-time',
+                'duration',
+                'mute',
+                'volume',
+                'download'
+            ]
+        });
+        // initialize playlist and controls
+        var index = 0,
+            playing = false,
+            mediaPath = 'soundfiles/',
+            extension = '',
+            tracks = [{
+                "track": 1,
+                "name": "Mitwa",
+                "duration": "0:57",
+                "file": "mitwa"
+            }, {
+                "track": 2,
+                "name": "Pani Da Rang",
+                "duration": "0:46",
+                "file": "Pani da rang"
+            },{
+                "track": 3,
+                "name": "Sapna Jahan",
+                "duration": "1:29",
+                "file": "sapna"
+            },{
+                "track": 4,
+                "name": "Teri Hogaiyaan 2",
+                "duration": "1:46",
+                "file": "Teri Hogaiyaan"
+            },{
+                "track": 5,
+                "name": "Baarish",
+                "duration": "1:13",
+                "file": "baarish"
+            },{
+                "track": 6,
+                "name": "Jab Tak",
+                "duration": "1:13",
+                "file": "jabtak"
+            },{
+                "track": 7,
+                "name": "Kuch toh hai",
+                "duration": "1:08",
+                "file": "kuchtohai"
+            }],
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackDuration = value.duration;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                }
+                $('#plList').append('<li> \
+                    <div class="plItem"> \
+                        <span class="plNum">' + trackNumber + '.</span> \
+                        <span class="plTitle">' + trackName + '</span> \
+                        <span class="plLength">' + trackDuration + '</span> \
+                    </div> \
+                </li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').on('play', function () {
+                playing = true;
+                npAction.text('Now Playing...');
+            }).on('pause', function () {
+                playing = false;
+                npAction.text('Paused...');
+            }).on('ended', function () {
+                npAction.text('Paused...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').on('click', function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').on('click', function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').on('click', function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+                updateDownload(id, audio.src);
+            },
+            updateDownload = function (id, source) {
+                player.on('loadedmetadata', function () {
+                    $('a[data-plyr="download"]').attr('href', source);
+                });
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    } else {
+        // no audio support
+        $('.column').addClass('hidden');
+        var noSupport = $('#audio1').text();
+        $('.container').append('<p class="no-support">' + noSupport + '</p>');
+    }
 });
